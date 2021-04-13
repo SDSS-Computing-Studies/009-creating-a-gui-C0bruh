20fa5c88e4657ede3e421ec8ec7686394a1054b3
C0bruh <78512228+C0bruh@users.noreply.github.com>
2021-04-13 09:40:46 -0700

    .

--- task1.py
+++ task1.py
@@ -2,8 +2,25 @@
 
 import tkinter as tk 
 from tkinter import *
-from tkinter import ttk
 
 window = tk.Tk()
 window.title("tk")
-window.geometry("600x200")
+window.geometry("600x50")
+
+nF = Frame()
+
+fentry1 = tk.Entry(nF,text="num 1")
+flabel1 = tk.Label(nF,text="x", borderwidth=6)
+fentry2 = tk.Entry(nF,text="num 2")
+fbutton1 = tk.Button(nF,text="=")
+fentry3 = tk.Entry(nF,text="answer")
+
+nF.pack()
+fentry1.pack(side=LEFT)
+flabel1.pack(side=LEFT)
+fentry2.pack(side=LEFT)
+fbutton1.pack(side=LEFT)
+fentry3.pack(side=LEFT)
+
+
+window.mainloop()
